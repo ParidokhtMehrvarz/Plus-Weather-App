9e6af8c476832d7bf738d82843ca507b12bc8a65
ParidokhtMehrvarz <155663015+ParidokhtMehrvarz@users.noreply.github.com>
2024-01-09 19:18:27 +0330

    Added icon image

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function refreshWeather(response) {
   let winSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
@@ -13,6 +14,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   winSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
